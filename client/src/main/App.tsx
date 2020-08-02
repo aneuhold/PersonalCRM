c8aad2f0ca7a0c348e056e5576306756e68a45ff
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-08-02 11:20:38 -0700

    I10 Convert FAB to speed dial component

--- client/src/main/App.tsx
+++ client/src/main/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Theme, createStyles, withStyles, WithStyles } from '@material-ui/core';
 import TopMenuBar from './components/TopMenuBar';
+import DocFab from './components/DocFab';
 
-function App() {
+function styles(theme: Theme) {
+  return createStyles({
+    fab: {
+      position: 'absolute',
+      right: theme.spacing(3),
+      bottom: theme.spacing(3),
+    },
+  });
+}
+
+type AppProps = WithStyles<typeof styles>;
+
+function App(props: AppProps) {
+  const { classes } = props;
   return (
     <div className="App">
       <TopMenuBar />
+      <DocFab className={classes.fab} />
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
--- client/src/main/components/DocFab.tsx
+++ client/src/main/components/DocFab.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
-import { Fab } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
+import React, { useState } from 'react';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import BusinessIcon from '@material-ui/icons/Business';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@material-ui/lab';
 
 interface DocFabProps {
-  className: string;
+  className?: string;
 }
+
+const actions = [
+  { icon: <PersonAddIcon />, name: 'Add Contact' },
+  { icon: <PlaylistAddIcon />, name: 'Add Task' },
+  { icon: <AssignmentIcon />, name: 'Add Opportunity' },
+  { icon: <BusinessIcon />, name: 'Add Manufacturer' },
+  { icon: <AccountTreeIcon />, name: 'Add Account' },
+];
 
 function DocFab(props: DocFabProps): JSX.Element {
   const { className } = props;
+  const [open, setOpen] = useState(false);
+
   return (
     <div className={className}>
-      <Fab color="primary" aria-label="add">
-        <AddIcon />
-      </Fab>
+      <SpeedDial
+        ariaLabel="Add document"
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map(action => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={() => {
+              setOpen(false);
+            }}
+          />
+        ))}
+      </SpeedDial>
     </div>
   );
 }
